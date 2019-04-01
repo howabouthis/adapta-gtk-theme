f7b8f404fc2b82b09f0ef47c19036b8e2160e67d
howabouthis <howabouthis@users.noreply.github.com>
2019-04-01 22:44:52 +0300

    Add helper script for autogen

---
+++ autogen-helper.sh
@@ -0,0 +1,2 @@
+./autogen.sh --prefix=/usr --enable-parallel --disable-gnome --disable-cinnamon --disable-flashback --disable-mate --disable-openbox --with-selection_color=#3F51B5 --with-accent_color=#64B5F6 --with-suggestion_color=#2196F3
+
